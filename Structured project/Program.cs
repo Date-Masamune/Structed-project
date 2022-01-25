a8cb1e404ff16d8ee7bf037154e44c232c0ff2c0
jacob gishwiller <sirbruce13@yahoo.com>
2022-01-25 10:07:52 -0700

    main

---
+++ Structured project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Structured_project
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello Jacob!");
+         
+      }
+   }
+}
